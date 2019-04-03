689510c2f707c87800663e387ee92279b8a7a54b
Dharani Dhar Bhoi <dharanidhar.bhoi@factset.com>
2019-04-03 23:44:30 +0530

    Spring Boot Main Application
    Spring Boot Main Application

---
+++ src/main/java/com/dharani/spring/starter/SpringVueMain.java
@@ -0,0 +1,14 @@
+package com.dharani.spring.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVueMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVueMain.class, args);
+
+	}
+
+}
