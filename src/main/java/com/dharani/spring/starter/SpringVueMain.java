64057eaf89250416c5de6c0a26a79411edaac0ad
Dharani Dhar Bhoi <dharanidhar.bhoi@factset.com>
2019-04-06 09:24:28 +0530

    Main Controller for Spring Vue project

--- src/main/java/com/dharani/spring/starter/SpringVueMain.java
+++ src/main/java/com/dharani/spring/starter/SpringVueMain.java
@@ -1,7 +1,15 @@
 package com.dharani.spring.starter;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class SpringVueMain {
@@ -10,5 +18,22 @@ public class SpringVueMain {
 		SpringApplication.run(SpringVueMain.class, args);
 
 	}
+	
+	
+	 // Fix the CORS errors
+    @Bean
+    public FilterRegistrationBean simpleCorsFilter() {  
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();  
+        CorsConfiguration config = new CorsConfiguration();  
+        config.setAllowCredentials(true); 
+        // *** URL below needs to match the Vue client URL and port ***
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:8080")); 
+        config.setAllowedMethods(Collections.singletonList("*"));  
+        config.setAllowedHeaders(Collections.singletonList("*"));  
+        source.registerCorsConfiguration("/**", config);  
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);  
+        return bean;  
+    }   
 
 }
